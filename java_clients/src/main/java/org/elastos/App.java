6b9fdc751f03604e960c529b7403181549df7ca8
xiaoming <1406629722@qq.com>
2019-02-28 14:34:48 +0800

    Java client skeleton
    See also: #6

---
+++ java_clients/src/main/java/org/elastos/App.java
@@ -0,0 +1,13 @@
+package org.elastos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
